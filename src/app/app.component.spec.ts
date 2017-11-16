c8e3fdb7a38bbe30f14f282030c79728eddb253c
xujiyuan <siyang@Siyangs-MacBook-Pro.local>
2017-11-16 00:04:30 -0500

    initial commit

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,5 +1,8 @@
-import { TestBed, async } from '@angular/core/testing';
-import { AppComponent } from './app.component';
+import {TestBed, async} from '@angular/core/testing';
+import {AppComponent} from './app.component';
+import {HttpClient, HttpHandler} from '@angular/common/http';
+import {HttpClientModule} from '@angular/common/http';
+
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -7,21 +10,84 @@ describe('AppComponent', () => {
         AppComponent
       ],
     }).compileComponents();
+    this.course = {
+      enrollmentCapacity: 100,
+      reservations: [
+        {
+          reservationCapacity: 25,
+          sequenceId: '1',
+          effectiveStartDate: '2017-12-01'
+        },
+        {
+          reservationCapacity: 25,
+          sequenceId: '2',
+          effectiveStartDate: '2017-12-01'
+        }
+      ],
+      currentEnrollment: {
+        effectiveDate: '2017-12-02',
+        reservedSeatsEnrolled: 20,
+        openSeatsEnrolled: 10
+      }
+    };
+    this.app = new AppComponent(null); // We do not test HTTP transcation therefore give null should be fine
+    this.app.course = this.course;
+
   }));
-  it('should create the app', async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app).toBeTruthy();
+  // customized unit tests
+  it('Make sure the constructor correctly build the variables', async(() => {
+    expect(this.app.reservedAvailable).toEqual(0);
+    expect(this.app.regularAvailable).toEqual(0);
   }));
-  it(`should have as title 'app'`, async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('app');
+
+  it('should default the total enrollment seats to 100 from test data', async(() => {
+    expect(this.app.calculateAvailableSeats(this.course).total).toEqual(100);
   }));
-  it('should render title in a h1 tag', async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to app!');
+
+  it('should default the total regular available seats to 40 from test data', async(() => {
+    expect(this.app.calculateAvailableSeats(this.course).regularAvailable).toEqual(40);
   }));
+
+  it('should default the total reserved available seats to 30 from test data', async(() => {
+    expect(this.app.calculateAvailableSeats(this.course).reservedAvailable).toEqual(30);
+  }));
+
+  it('should default the total reserved taken seats to 20 from test data', async(() => {
+    expect(this.app.calculateAvailableSeats(this.course).reserveTaken).toEqual(20);
+  }));
+
+  it('should default the total regular taken seats to 10 from test data', async(() => {
+    expect(this.app.calculateAvailableSeats(this.course).regularTaken).toEqual(10);
+  }));
+
+  it('should not calculate reservations that is not effect yet', async(() => {
+    const testCourse = Object.assign(this.course); // make a deep copy of course object
+    testCourse['reservations'][0].effectiveStartDate = '2019-12-01';
+    const report = this.app.calculateAvailableSeats(testCourse);
+
+    expect(report.reservedAvailable).toEqual(5);
+    expect(report.reserveTaken).toEqual(20);
+    expect(report.total).toEqual(100);
+    expect(report.regularAvailable).toEqual(65);
+    expect(report.regularTaken).toEqual(10);
+
+  }));
+
+  it('should work with additional reservations ', async(() => {
+    const testCourse = Object.assign(this.course); // make a deep copy of course object
+    testCourse['reservations'].push(
+      {
+        reservationCapacity: 10,
+        sequenceId: '3',
+        effectiveStartDate: '2017-11-01'
+      }
+    );
+    const report = this.app.calculateAvailableSeats(testCourse);
+    expect(report.reservedAvailable).toEqual(40);
+    expect(report.reserveTaken).toEqual(20);
+    expect(report.total).toEqual(100);
+    expect(report.regularAvailable).toEqual(30);
+    expect(report.regularTaken).toEqual(10);
+  }));
+
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import * as _ from 'lodash';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,62 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+
+  // declare variables
+  reservedAvailable: number;
+  regularAvailable: number;
+  course: Object = {};
+  report: Object = {};
+
+  constructor(private http: HttpClient) {
+    this.reservedAvailable = 0;
+    this.regularAvailable = 0;
+    // this.course = JSON.parse('course.json');
+
+    if (this.http) {
+      this.http.get('assets/course.json').subscribe(data => {
+        if (data) {
+          this.course = data;
+          this.report = this.calculateAvailableSeats(null);
+        }
+
+      }, err => {
+        console.log('Could not get the correct json file' + err);
+      });
+    }
+
+  }
+
+  calculateAvailableSeats(course) {
+    // add a parameter to function signature to isolate testing functionalities
+    if (!course) {
+      course = this.course;
+    }
+    if (course) {
+      this.regularAvailable = course['enrollmentCapacity'];
+
+      _.forEach(course['reservations'], (reservation) => {
+        if (new Date(course['currentEnrollment'].effectiveDate) > new Date(reservation.effectiveStartDate)) {
+          this.reservedAvailable += reservation.reservationCapacity;
+          reservation.status = 'Active';
+        }
+        else {
+          reservation.status = 'Inactive';
+        }
+
+
+      });
+      this.regularAvailable = this.regularAvailable - (this.reservedAvailable + course['currentEnrollment'].openSeatsEnrolled);
+      this.reservedAvailable = this.reservedAvailable - course['currentEnrollment'].reservedSeatsEnrolled;
+
+      return {
+        total: course['enrollmentCapacity'],
+        reservedAvailable: this.reservedAvailable,
+        regularAvailable: this.regularAvailable,
+        reserveTaken: course['currentEnrollment'].reservedSeatsEnrolled,
+        regularTaken: course['currentEnrollment'].openSeatsEnrolled
+      };
+    }
+    return null;
+  }
 }
